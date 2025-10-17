430758651c6cd9884e53388ec83737754d6c63b3
Ravindu Sandeep <157776061+SandeepRavindu@users.noreply.github.com>
2025-10-17 20:30:33 +0530

    Add allowed origin for production frontend

--- src/main/java/com/creatorboost/auth_service/config/SecurityConfig.java
+++ src/main/java/com/creatorboost/auth_service/config/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
 
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5174","https://creatorboost-frontend-users.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
